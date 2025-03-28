40071e5c4760a784392b457413c8a6140c56b68e
heum23 <jinsoon6230@naver.com>
2025-03-28 20:23:33 +0900

    12

--- auction/src/app.module.ts
+++ auction/src/app.module.ts
@@ -6,8 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
-
-import { Users } from './entities/users.entity'
+import { Users } from './entities/users.entity';
 import { UserCheck } from './entities/user_check';
 import { Vehicles } from './entities/vehicles';
 import { Grades } from './entities/grades';
@@ -17,26 +16,42 @@ import { Payment } from './entities/payment';
 import { Favorites } from './entities/favorites';
 import { Admins } from './entities/admins';
 import { Notifications } from './entities/notifications';
+
 @Module({
   imports: [
-    ConfigModule.forRoot({ // 환경변수 설정 (배포, 개발 구분)
+    ConfigModule.forRoot({
+      // 환경변수 설정 (배포, 개발 구분)
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development',
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development',
     }),
-
-    TypeOrmModule.forRoot({ // TypeORM 설정 (db 연결)
+    TypeOrmModule.forRoot({
+      // TypeORM 설정 (db 연결)
       type: 'mysql',
       host: 'localhost',
       port: Number(process.env.DB_PORT) || 3306,
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
-      entities: [ Users, UserCheck, Vehicles, Grades, Auctions, Bids, Payment, Favorites, Admins, Notifications ],
+      entities: [
+        Users,
+        UserCheck,
+        Vehicles,
+        Grades,
+        Auctions,
+        Bids,
+        Payment,
+        Favorites,
+        Admins,
+        Notifications,
+      ],
       charset: 'utf8mb4',
       synchronize: process.env.NODE_ENV !== 'production', // 개발 환경에서만 true
     }),
     UsersModule,
-    AuthModule
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
