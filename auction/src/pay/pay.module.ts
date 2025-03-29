597cf57e3eec34226422e26b754d57cd146d77e6
Yzoraa <yezing01@naver.com>
2025-03-30 02:02:49 +0900

    Feat: 마이페이지 요청 완료2

--- auction/src/pay/pay.module.ts
+++ auction/src/pay/pay.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PayService } from './pay.service';
 import { PayController } from './pay.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from 'src/entities/payment';
+import { Users } from 'src/entities/users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Payment, Users])],
   providers: [PayService],
   controllers: [PayController]
 })
