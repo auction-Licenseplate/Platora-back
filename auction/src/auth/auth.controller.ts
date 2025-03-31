3104167764484dba8a8894647d8132bb1491651b
heum23 <jinsoon6230@naver.com>
2025-03-31 19:55:41 +0900

    idfind

--- auction/src/auth/auth.controller.ts
+++ auction/src/auth/auth.controller.ts
@@ -1,103 +1,128 @@
-import { Body, Controller, Get, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService){}
+  constructor(private readonly authService: AuthService) {}
 
-    // 회원가입 API
-    @Post('/signup')
-    async signUp(@Body() body: {email: string; password: string; name: string; phone: string}) {
-        return this.authService.signUp(body.email, body.password, body.name, body.phone)
+  // 회원가입 API
+  @Post('/signup')
+  async signUp(
+    @Body()
+    body: {
+      email: string;
+      password: string;
+      name: string;
+      phone: string;
+    },
+  ) {
+    return this.authService.signUp(
+      body.email,
+      body.password,
+      body.name,
+      body.phone,
+    );
+  }
+
+  // 로컬 로그인 API
+  @Post('/login')
+  @UseGuards(AuthGuard('local'))
+  async login(@Req() req: any, @Res() res: any) {
+    const { id, email, token } = req.user;
+    if (!token) {
+      return { message: '로그인 실패' };
     }
 
-    // 로컬 로그인 API
-    @Post('/login')
-    @UseGuards(AuthGuard('local'))
-    async login(@Req() req: any, @Res() res: any) {
-        const { id, email, token } = req.user;
-        if (!token) {
-            return { message: '로그인 실패' }; 
-        }
+    // 쿠키에 토큰 저장
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24, // 1일 유지
+    });
 
-        // 쿠키에 토큰 저장
-        res.cookie('access_token', token, {
-            httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24, // 1일 유지
-        });
-  
-        // 로그인 성공 응답
-        return res.json({ message: '로그인 성공', id, email });
-    }
-    
-    // 카카오 로그인 API
-    @Get('/kakao')
-    @UseGuards(AuthGuard('kakao'))
-    async kakaoLogin(@Req() req: Request){
-        // 카카오 로그인 페이지로 자동 리다이렉트
-    }
+    // 로그인 성공 응답
+    return res.json({ message: '로그인 성공', id, email });
+  }
 
-    // 네이버 로그인 API
-    @Get('/naver')
-    @UseGuards(AuthGuard('naver'))
-    async naverLogin(@Req() req: Request){
-        // 네이버 로그인 페이지로 자동 리다이렉트
-    }
+  // 카카오 로그인 API
+  @Get('/kakao')
+  @UseGuards(AuthGuard('kakao'))
+  async kakaoLogin(@Req() req: Request) {
+    // 카카오 로그인 페이지로 자동 리다이렉트
+  }
 
-    // 구글 로그인 API
-    @Get('/google')
-    @UseGuards(AuthGuard('google'))
-    async googleLogin(): Promise<void>{
-        // 구글 로그인 페이지로 자동 리다이렉트
-    }
+  // 네이버 로그인 API
+  @Get('/naver')
+  @UseGuards(AuthGuard('naver'))
+  async naverLogin(@Req() req: Request) {
+    // 네이버 로그인 페이지로 자동 리다이렉트
+  }
 
-    // sns 로그인 타입 구별
-    @Post('login/:type')
-    async socialLogin(@Param('type') type: string, @Body() body:{code: string}){
-        console.log(`프론트에서 받은타입 ${type}`);
-        console.log(`프론트에서 받은코드 ${body.code}`);
-        let user;
+  // 구글 로그인 API
+  @Get('/google')
+  @UseGuards(AuthGuard('google'))
+  async googleLogin(): Promise<void> {
+    // 구글 로그인 페이지로 자동 리다이렉트
+  }
 
-        if(type === 'kakao'){
-            console.log('카카오 호출됨');
-            user = await this.authService.kakaoUser(body.code);
-        }
+  // sns 로그인 타입 구별
+  @Post('login/:type')
+  async socialLogin(
+    @Param('type') type: string,
+    @Body() body: { code: string },
+  ) {
+    console.log(`프론트에서 받은타입 ${type}`);
+    console.log(`프론트에서 받은코드 ${body.code}`);
+    let user;
 
-        if(type === 'naver') {
-            console.log('네이버 호출됨');
-            // user = await this.authService.naverUser(body.code);
-        }
-        
-        if(type === 'google'){
-            console.log('구글 호출됨');
-            user = await this.authService.googleUser(body.code);
-        }
-
-        if(!user) {
-            return {message: '200 소셜로그인 실패'};
-        }
-
-        const token = await this.authService.snsToken(user);
-
-        return {
-            message: `${type} 로그인 성공`,
-            user,
-            accessToken: token.accessToken,
-            refreshToken: token.refreshToken,
-        }
+    if (type === 'kakao') {
+      console.log('카카오 호출됨');
+      user = await this.authService.kakaoUser(body.code);
     }
 
-    // 아이디 찾기
-    @Post('/findID')
-    async userID(@Body() body: {email: string; password: string}){
-        return await this.authService.findId(body.email, body.password)
+    if (type === 'naver') {
+      console.log('네이버 호출됨');
+      // user = await this.authService.naverUser(body.code);
     }
 
-    // 비밀번호 찾기
-    @Post('/findpw')
-    async userPW(@Body() body){
-        const { email, phone } = body;
-        return await this.authService.findPW(email, phone);
+    if (type === 'google') {
+      console.log('구글 호출됨');
+      user = await this.authService.googleUser(body.code);
     }
+
+    if (!user) {
+      return { message: '200 소셜로그인 실패' };
+    }
+
+    const token = await this.authService.snsToken(user);
+
+    return {
+      message: `${type} 로그인 성공`,
+      user,
+      accessToken: token.accessToken,
+      refreshToken: token.refreshToken,
+    };
+  }
+
+  // 아이디 찾기
+  @Post('/findID')
+  async userID(@Body() body: { name: string; phone: string }) {
+    return await this.authService.findId(body.name, body.phone);
+  }
+
+  // 비밀번호 찾기
+  @Post('/findpw')
+  async userPW(@Body() body) {
+    const { email, phone } = body;
+    return await this.authService.findPW(email, phone);
+  }
 }
--- auction/src/auth/auth.service.ts
+++ auction/src/auth/auth.service.ts
@@ -8,185 +8,206 @@ import axios from 'axios';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly jwtService: JwtService,
-        @InjectRepository(Users)
-        private userRepository: Repository<Users>,
-    ) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(Users)
+    private userRepository: Repository<Users>,
+  ) {}
 
-    // 일반 회원가입
-    async signUp(email: string, password: string, name: string, phone: string) {
-        // 이메일 중복 체크
-        const existingUser = await this.userRepository.findOne({where: {email}});
-        if(existingUser) {
-            throw new Error('이미 존재하는 이메일입니다.');
-        }
-        
-        const hashPW = await bcrypt.hash(password, 10);
-        const newUser = this.userRepository.create({
-            email,
-            password: hashPW,
-            name,
-            phone
-        })
-
-        await this.userRepository.save(newUser); // db에 저장
-        return { message: '회원가입 성공' };
+  // 일반 회원가입
+  async signUp(email: string, password: string, name: string, phone: string) {
+    // 이메일 중복 체크
+    const existingUser = await this.userRepository.findOne({
+      where: { email },
+    });
+    if (existingUser) {
+      throw new Error('이미 존재하는 이메일입니다.');
     }
 
-    // 일반 로그인
-    async localLogin(email: string, password: string) {
-        // 유저 정보 확인
-        const user = await this.userRepository.findOne({ where:{email}});
-        if(!user) {
-            throw new UnauthorizedException('200 유저 정보 없음.');
-        }
+    const hashPW = await bcrypt.hash(password, 10);
+    const newUser = this.userRepository.create({
+      email,
+      password: hashPW,
+      name,
+      phone,
+    });
 
-        // 비밀번호 검증 (느낌표는 null일 가능성이 있는 것처럼 보이더라도 실제로 null이 아니라는 것을 알려줌)
-        const match = await bcrypt.compare(password, user.password!);
-        if(!match) {
-            return { message: '200 비밀번호 불일치함' };
-        }
+    await this.userRepository.save(newUser); // db에 저장
+    return { message: '회원가입 성공' };
+  }
 
-        // 토큰 발급
-        const payload = {id: user.id, email: user.email};
-        const token = this.jwtService.sign(payload);
-
-        // localstrategy에서 req.user로 데이터 넘김
-        return { id: user.id, email: user.email, token };
+  // 일반 로그인
+  async localLogin(email: string, password: string) {
+    // 유저 정보 확인
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (!user) {
+      throw new UnauthorizedException('200 유저 정보 없음.');
     }
 
-    // 카카오 로그인
-    // async kakaoUser(kakao_account: string) {
-    //     let user = await this.userRepository.findOne({ where: { email: kakao_account } });
-    
-    //     if (!user) { // 유저가 없으면 새로 생성
-    //         user = this.userRepository.create({
-    //             email: kakao_account,
-    //             provider: 'kakao',
-    //         });
-    //         await this.userRepository.save(user);
-    //     }
-    
-    //     return user;
-    // }
-    
-    async kakaoUser(code: string) {
-        console.log("카카오 클라이언트 아이디:", process.env.KAKAO_CLIENT_ID);
-        console.log("카카오 콜백 주소:", process.env.KAKAO_REDIRECT_URL);
-
-        const KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
-        const KAKAO_USER_INFO_URL = "https://kapi.kakao.com/v2/user/me";
-    
-        // 1. 인가 코드로 카카오에서 액세스 토큰 받아오기
-        const tokenResponse = await axios.post(KAKAO_TOKEN_URL, null, {
-            params: {
-                // grant_type: "authorization_code",
-                client_id: process.env.KAKAO_CLIENT_ID,
-                client_secret: process.env.KAKAO_CLIENT_SECRET,
-                redirect_uri: process.env.KAKAO_REDIRECT_URL,
-                code: code,
-            },
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-        }).catch(error => {
-            console.error("카카오 토큰 요청 실패:", error.response?.data || error.message);
-        });
-    
-        const accessToken = tokenResponse?.data.access_token;
-    
-        // 2. 액세스 토큰으로 사용자 정보 받아오기
-        const userResponse = await axios.get(KAKAO_USER_INFO_URL, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-        }).catch(error => {
-            console.error("카카오 사용자 정보 요청 실패:", error.response?.data || error.message);
-        });
-    
-        const kakaoAccount = userResponse?.data.kakao_account;
-    
-        let user = await this.userRepository.findOne({
-            where: { email: kakaoAccount.email },
-        });
-    
-        if (!user) {
-            user = this.userRepository.create({
-                email: kakaoAccount.email,
-                provider: "kakao",
-            });
-            await this.userRepository.save(user);
-        }
-        console.log("카카오 사용자 정보:", userResponse?.data);
-    
-        return user;
+    // 비밀번호 검증 (느낌표는 null일 가능성이 있는 것처럼 보이더라도 실제로 null이 아니라는 것을 알려줌)
+    const match = await bcrypt.compare(password, user.password!);
+    if (!match) {
+      return { message: '200 비밀번호 불일치함' };
     }
 
-    // 네이버 로그인
-    async naverUser(naver_account: string, name: string, phone: string){
-        let user = await this.userRepository.findOne({ where: { email: naver_account } });
-        if (!user) { // 유저가 없으면 새로 생성
-            user = this.userRepository.create({
-                email: naver_account,
-                name,
-                phone,
-                provider: 'naver',
-            });
-            await this.userRepository.save(user);
-        }
-        return user;
+    // 토큰 발급
+    const payload = { id: user.id, email: user.email };
+    const token = this.jwtService.sign(payload);
+
+    // localstrategy에서 req.user로 데이터 넘김
+    return { id: user.id, email: user.email, token };
+  }
+
+  // 카카오 로그인
+  // async kakaoUser(kakao_account: string) {
+  //     let user = await this.userRepository.findOne({ where: { email: kakao_account } });
+
+  //     if (!user) { // 유저가 없으면 새로 생성
+  //         user = this.userRepository.create({
+  //             email: kakao_account,
+  //             provider: 'kakao',
+  //         });
+  //         await this.userRepository.save(user);
+  //     }
+
+  //     return user;
+  // }
+
+  async kakaoUser(code: string) {
+    console.log('카카오 클라이언트 아이디:', process.env.KAKAO_CLIENT_ID);
+    console.log('카카오 콜백 주소:', process.env.KAKAO_REDIRECT_URL);
+
+    const KAKAO_TOKEN_URL = 'https://kauth.kakao.com/oauth/token';
+    const KAKAO_USER_INFO_URL = 'https://kapi.kakao.com/v2/user/me';
+
+    // 1. 인가 코드로 카카오에서 액세스 토큰 받아오기
+    const tokenResponse = await axios
+      .post(KAKAO_TOKEN_URL, null, {
+        params: {
+          // grant_type: "authorization_code",
+          client_id: process.env.KAKAO_CLIENT_ID,
+          client_secret: process.env.KAKAO_CLIENT_SECRET,
+          redirect_uri: process.env.KAKAO_REDIRECT_URL,
+          code: code,
+        },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .catch((error) => {
+        console.error(
+          '카카오 토큰 요청 실패:',
+          error.response?.data || error.message,
+        );
+      });
+
+    const accessToken = tokenResponse?.data.access_token;
+
+    // 2. 액세스 토큰으로 사용자 정보 받아오기
+    const userResponse = await axios
+      .get(KAKAO_USER_INFO_URL, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .catch((error) => {
+        console.error(
+          '카카오 사용자 정보 요청 실패:',
+          error.response?.data || error.message,
+        );
+      });
+
+    const kakaoAccount = userResponse?.data.kakao_account;
+
+    let user = await this.userRepository.findOne({
+      where: { email: kakaoAccount.email },
+    });
+
+    if (!user) {
+      user = this.userRepository.create({
+        email: kakaoAccount.email,
+        provider: 'kakao',
+      });
+      await this.userRepository.save(user);
+    }
+    console.log('카카오 사용자 정보:', userResponse?.data);
+
+    return user;
+  }
+
+  // 네이버 로그인
+  async naverUser(naver_account: string, name: string, phone: string) {
+    let user = await this.userRepository.findOne({
+      where: { email: naver_account },
+    });
+    if (!user) {
+      // 유저가 없으면 새로 생성
+      user = this.userRepository.create({
+        email: naver_account,
+        name,
+        phone,
+        provider: 'naver',
+      });
+      await this.userRepository.save(user);
+    }
+    return user;
+  }
+
+  // 구글 로그인
+  async googleUser(google_account: string) {
+    let user = await this.userRepository.findOne({
+      where: { email: google_account },
+    });
+
+    if (!user) {
+      // 유저가 없으면 새로 생성
+      user = this.userRepository.create({
+        email: google_account,
+        provider: 'google',
+      });
+      await this.userRepository.save(user);
     }
 
-    // 구글 로그인
-    async googleUser(google_account: string){
-        let user = await this.userRepository.findOne({ where: { email: google_account } });
-    
-        if (!user) { // 유저가 없으면 새로 생성
-            user = this.userRepository.create({
-                email: google_account,
-                provider: 'google',
-            });
-            await this.userRepository.save(user);
-        }
+    return user;
+  }
 
-        return user;
+  // sns JWT 토큰 발급
+  async snsToken(user: any) {
+    const payload = {
+      id: user.id,
+      email: user.email,
+      provider: user.provider,
+    };
+    const accessToken = this.jwtService.sign(payload, { expiresIn: '1h' });
+    const refreshToken = this.jwtService.sign(payload, { expiresIn: '7d' });
+
+    // refresh 토큰 db 저장?
+    // user.refreshToken = refreshToken;
+    // await this.userRepository.save(user);
+
+    return { accessToken, refreshToken };
+  }
+
+  // 아이디 찾기
+  async findId(name: string, phone: string) {
+    console.log(name, phone);
+    const user = await this.userRepository.findOne({
+      where: { name, phone },
+    });
+    if (!user) {
+      return { message: '저장된 아이디 없음' };
+    }
+    console.log(user);
+    return { email: user.email };
+  }
+  // 비밀번호 찾기
+  async findPW(email: string, phone: string) {
+    const user = await this.userRepository.findOne({ where: { email, phone } });
+    if (!user) {
+      return { message: '저장된 비밀번호 없음' };
     }
 
-    // sns JWT 토큰 발급
-    async snsToken(user: any){
-        const payload = { 
-            id: user.id,
-            email: user.email, 
-            provider: user.provider 
-        };
-        const accessToken = this.jwtService.sign(payload, { expiresIn: "1h" });
-        const refreshToken = this.jwtService.sign(payload, { expiresIn: "7d" });
-        
-        // refresh 토큰 db 저장?
-        // user.refreshToken = refreshToken;
-        // await this.userRepository.save(user);
-
-        return { accessToken, refreshToken };
-    }
-
-    // 아이디 찾기
-    async findId(email:string, password:string){
-        const user = await this.userRepository.findOne({where: {email, password}});
-        if(!user){
-            return { message: '저장된 아이디 없음' };
-        }
-
-        return { userID: user.id };
-    }
-    // 비밀번호 찾기
-    async findPW(email:string, phone:string){
-        const user = await this.userRepository.findOne({where: {email, phone}});
-        if(!user){
-            return { message: '저장된 비밀번호 없음' };
-        }
-
-        return { userID: user.id };
-    }
+    return { userID: user.id };
+  }
 }
