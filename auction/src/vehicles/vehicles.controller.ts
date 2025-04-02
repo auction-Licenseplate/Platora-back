637fbe9b93ca4131f482551d528e0419c93a6c47
heum23 <jinsoon6230@naver.com>
2025-04-02 19:06:38 +0900

    ai

--- auction/src/vehicles/vehicles.controller.ts
+++ auction/src/vehicles/vehicles.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards, Post, Body } from '@nestjs/common';
 import { VehiclesService } from './vehicles.service';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
 
@@ -12,5 +12,22 @@ export class VehiclesController {
   async carOwnership(@Req() req) {
     const userId = req.user.id;
     return this.vehicleService.getCarData(userId);
+  }
+
+  // 재미나이 AI 채팅 API 호출
+  @Post('/aichat')
+  async createMessage(@Body() content: { message: string }) {
+    if (!content.message) {
+      return { error: '메시지를 입력해야 합니다.' };
+    }
+
+    try {
+      const response = await this.vehicleService.chatWithZeminar(
+        content.message,
+      );
+      return response;
+    } catch (error) {
+      return { error: 'AI 응답을 가져오는 데 실패했습니다.' };
+    }
   }
 }
