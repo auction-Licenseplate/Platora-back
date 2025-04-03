6105ef7af1a938c9ba425b9fdd964fcdaa812e3f
heum23 <jinsoon6230@naver.com>
2025-04-03 20:14:57 +0900

    admin

--- auction/src/admins/admins.controller.ts
+++ auction/src/admins/admins.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { AdminsService } from './admins.service';
 @Controller('admins')
-export class AdminsController {}
+export class AdminsController {
+  constructor(private readonly adminService: AdminsService) {}
+
+  @Get('/userinfo')
+  async userinfo(
+    @Body()
+    body: {
+      email: string;
+      password: string;
+      name: string;
+      phone: string;
+    },
+  ) {
+    return this.adminService.userinfo();
+  }
+}
