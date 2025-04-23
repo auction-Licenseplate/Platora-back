bb5ab5c2e5a7794d7b23fbe61b9d349fccedcd40
Yzoraa <yezing01@naver.com>
2025-04-23 19:11:41 +0900

    ㄹㅇ

--- auction/src/auth/strategies/jwt.strategy.ts
+++ auction/src/auth/strategies/jwt.strategy.ts
@@ -12,14 +12,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private userRepository: Repository<Users>,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        (request) => {
-          console.log("쿠키에서 토큰 추출:", request?.cookies);
-          const token = request?.cookies?.accessToken; // 헤더 Bearer Token 추출
-          console.log(token, '토큰자름')
-          return token;
-        },
-      ]),
+      // jwtFromRequest: ExtractJwt.fromExtractors([
+      //   (request) => {
+      //     console.log(request, '프론트요청')
+      //     console.log("쿠키에서 토큰 추출:", request?.cookies);
+      //     const token = request?.cookies?.accessToken; // 헤더 Bearer Token 추출
+      //     console.log(token, '토큰자름')
+      //     return token;
+      //   },
+      // ]),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET || 'defaultSecret',
     });
